66899245e37d03d8ee7fe2226a4f40135f3f1957
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-04-16 23:21:19 -0300

    Adiciona rota e url para amigos

--- src/routes.php
+++ src/routes.php
@@ -13,10 +13,11 @@ $router->post('/cadastro','LoginController@signUpAction');
 
 $router->post('/post/new', 'PostController@new');
 
+$router->get('/perfil/{id}/amigos', 'ProfileController@friends');
 $router->get('/perfil/{id}/follow', 'ProfileController@follow');
 $router->get('/perfil/{id}', 'ProfileController@index');
 $router->get('/perfil', 'ProfileController@index');
-
+$router->get('/amigos', 'ProfilController@friends');
 $router->get('/sair', 'LoginController@logout');
 
 //$router->get('/pesquisar');
