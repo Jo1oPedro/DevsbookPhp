41c33bef884d2ebadb28cedfcbef71985b243c42
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-04-20 05:49:33 -0300

    Adiciona html e metodo javascript async para upload de fotos

--- src/views/partials/feed-editor.php
+++ src/views/partials/feed-editor.php
@@ -6,6 +6,12 @@
             </div>
             <div class="feed-new-input-placeholder">O que você está pensando, <?=$user->name?>?</div>
             <div class="feed-new-input" contenteditable="true"></div>
+            <div class="feed-new-photo">
+                <label for="photo">
+                    <img src="<?=$base;?>/assets/images/photo.png">
+                </label>
+                <input type="file" id="photo" name="photo" class="feed-new-file" accept="image/png,image/jpg,image/jpeg">
+            </div>
             <div class="feed-new-send">
                 <img src="<?=$base;?>/assets/images/send.png" />
             </div>
@@ -20,6 +26,26 @@
 let feedInput = document.querySelector('.feed-new-input');
 let feedSubmit = document.querySelector('.feed-new-send');
 let feedForm = document.querySelector('.feed-new-form');
+let feedFile = document.querySelector('.feed-new-file');
+
+feedFile.addEventListener('click', async function() {
+    let photo = feedFile.files[0];
+    
+    let formData = new FormData();
+    formData.append('photo', photo);
+
+    let req = await fetch(BASE+'/ajax/upload', {
+        method: 'POST',
+        body: formData
+    });
+    let json = await req.json();
+
+    if(json.error != '') {
+        alert(json.error);
+    }
+
+    window.location.href = window.location.href;
+});
 
 feedSubmit.addEventListener('click', function(obj) {
     let value = feedInput.innerText.trim();
